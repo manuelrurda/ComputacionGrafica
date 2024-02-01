49dda8c31ebb9ea96d4efa19ae85f3ae049e04d7
Manuel Rodriguez Urdapilelta <manuelrurda@gmail.com>
2024-01-31 19:16:05 -0600

    Initial Commit

---
+++ Hello OpenGL/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Hello OpenGL
+//
+//  Created by Manuel Rodriguez Urdapilleta on 31/01/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
