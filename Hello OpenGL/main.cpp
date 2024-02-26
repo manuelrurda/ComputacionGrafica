9ab18127b509cdf97271e2c6d66c38640f5fd87f
Manuel Rodriguez Urdapilelta <manuelrurda@gmail.com>
2024-02-25 22:52:29 -0600

    Set up pre practica (Agregar shaders a build phases y working directore a schema xcode)

--- Hello OpenGL/main.cpp
+++ Hello OpenGL/main.cpp
@@ -1,82 +1,393 @@
-//
-//  main.cpp
-//  Hello OpenGL
-//
-//  Created by Manuel Rodriguez Urdapilleta on 31/01/24.
-//
-
+//pr·ctica 3: Modelado GeomÈtrico y C·mara SintÈtica.
 #include <stdio.h>
+#include <string.h>
+#include<cmath>
+#include<vector>
 #include <glew.h>
 #include <glfw3.h>
+//glm
+#include<glm.hpp>
+#include<gtc/matrix_transform.hpp>
+#include<gtc/type_ptr.hpp>
+#include <gtc/random.hpp>
+//clases para dar orden y limpieza al cÚdigo
+#include"Mesh.h"
+#include"Shader.h"
+#include"Sphere.h"
+#include"Window.h"
+#include"Camera.h"
+//tecla E: Rotar sobre el eje X
+//tecla R: Rotar sobre el eje Y
+//tecla T: Rotar sobre el eje Z
+
+
+using std::vector;
+
 //Dimensiones de la ventana
-const int WIDTH = 800, HEIGHT = 600;
+const float toRadians = 3.14159265f/180.0; //grados a radianes
+const float PI = 3.14159265f;
+GLfloat deltaTime = 0.0f;
+GLfloat lastTime = 0.0f;
+static double limitFPS = 1.0 / 60.0;
+Camera camera;
+Window mainWindow;
+vector<Mesh*> meshList;
+vector<Shader>shaderList;
+//Vertex Shader
+static const char* vShader = "shaders/shader.vert";
+static const char* fShader = "shaders/shader.frag";
+static const char* vShaderColor = "shaders/shadercolor.vert";
+Sphere sp = Sphere(1.0, 20, 20); //recibe radio, slices, stacks
 
-int main(void)
+
+
+
+void CrearCubo()
 {
-    //Inicialización de GLFW
-    if (!glfwInit())
-    {
-        printf("Falló inicializar GLFW");
-        glfwTerminate();
-        return -1;
+    unsigned int cubo_indices[] = {
+        // front
+        0, 1, 2,
+        2, 3, 0,
+        // right
+        1, 5, 6,
+        6, 2, 1,
+        // back
+        7, 6, 5,
+        5, 4, 7,
+        // left
+        4, 0, 3,
+        3, 7, 4,
+        // bottom
+        4, 5, 1,
+        1, 0, 4,
+        // top
+        3, 2, 6,
+        6, 7, 3
+    };
+
+    GLfloat cubo_vertices[] = {
+        // front
+        -0.5f, -0.5f,  0.5f,
+        0.5f, -0.5f,  0.5f,
+        0.5f,  0.5f,  0.5f,
+        -0.5f,  0.5f,  0.5f,
+        // back
+        -0.5f, -0.5f, -0.5f,
+        0.5f, -0.5f, -0.5f,
+        0.5f,  0.5f, -0.5f,
+        -0.5f,  0.5f, -0.5f
+    };
+    Mesh* cubo = new Mesh();
+    cubo->CreateMesh(cubo_vertices, cubo_indices, 24, 36);
+    meshList.push_back(cubo);
+}
+
+// Pir·mide triangular regular
+void CrearPiramideTriangular()
+{
+    unsigned int indices_piramide_triangular[] = {
+            0,1,2,
+            1,3,2,
+            3,0,2,
+            1,0,3
+
+    };
+    GLfloat vertices_piramide_triangular[] = {
+        -0.5f, -0.5f,0.0f,    //0
+        0.5f,-0.5f,0.0f,    //1
+        0.0f,0.5f, -0.25f,    //2
+        0.0f,-0.5f,-0.5f,    //3
+
+    };
+    Mesh* obj1 = new Mesh();
+    obj1->CreateMesh(vertices_piramide_triangular, indices_piramide_triangular, 12, 12);
+    meshList.push_back(obj1);
+
+}
+/*
+Crear cilindro, cono y esferas con arreglos din·micos vector creados en el Semestre 2023 - 1 : por S·nchez PÈrez Omar Alejandro
+*/
+void CrearCilindro(int res, float R) {
+
+    //constantes utilizadas en los ciclos for
+    int n, i;
+    //c·lculo del paso interno en la circunferencia y variables que almacenar·n cada coordenada de cada vÈrtice
+    GLfloat dt = 2 * PI / res, x, z, y = -0.5f;
+
+    vector<GLfloat> vertices;
+    vector<unsigned int> indices;
+
+    //ciclo for para crear los vÈrtices de las paredes del cilindro
+    for (n = 0; n <= (res); n++) {
+        if (n != res) {
+            x = R * cos((n)*dt);
+            z = R * sin((n)*dt);
+        }
+        //caso para terminar el cÌrculo
+        else {
+            x = R * cos((0)*dt);
+            z = R * sin((0)*dt);
+        }
+        for (i = 0; i < 6; i++) {
+            switch (i) {
+            case 0:
+                vertices.push_back(x);
+                break;
+            case 1:
+                vertices.push_back(y);
+                break;
+            case 2:
+                vertices.push_back(z);
+                break;
+            case 3:
+                vertices.push_back(x);
+                break;
+            case 4:
+                vertices.push_back(0.5);
+                break;
+            case 5:
+                vertices.push_back(z);
+                break;
+            }
+        }
     }
+
+    //ciclo for para crear la circunferencia inferior
+    for (n = 0; n <= (res); n++) {
+        x = R * cos((n)*dt);
+        z = R * sin((n)*dt);
+        for (i = 0; i < 3; i++) {
+            switch (i) {
+            case 0:
+                vertices.push_back(x);
+                break;
+            case 1:
+                vertices.push_back(-0.5f);
+                break;
+            case 2:
+                vertices.push_back(z);
+                break;
+            }
+        }
+    }
+
+    //ciclo for para crear la circunferencia superior
+    for (n = 0; n <= (res); n++) {
+        x = R * cos((n)*dt);
+        z = R * sin((n)*dt);
+        for (i = 0; i < 3; i++) {
+            switch (i) {
+            case 0:
+                vertices.push_back(x);
+                break;
+            case 1:
+                vertices.push_back(0.5);
+                break;
+            case 2:
+                vertices.push_back(z);
+                break;
+            }
+        }
+    }
+
+    //Se generan los indices de los vÈrtices
+    for (i = 0; i < vertices.size(); i++) indices.push_back(i);
+
+    //se genera el mesh del cilindro
+    Mesh *cilindro = new Mesh();
+    cilindro->CreateMeshGeometry(vertices, indices, vertices.size(), indices.size());
+    meshList.push_back(cilindro);
+}
+
+//funciÛn para crear un cono
+void CrearCono(int res,float R) {
+
+    //constantes utilizadas en los ciclos for
+    int n, i;
+    //c·lculo del paso interno en la circunferencia y variables que almacenar·n cada coordenada de cada vÈrtice
+    GLfloat dt = 2 * PI / res, x, z, y = -0.5f;
     
-    // Define version and compatibility settings
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-    glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    vector<GLfloat> vertices;
+    vector<unsigned int> indices;
 
-    //CREAR VENTANA
-    GLFWwindow* mainWindow;
-    mainWindow = glfwCreateWindow(800, 600, "Primer ventana", NULL, NULL);
-
-    if (!mainWindow)
-    {
-        printf("Fallo en crearse la ventana con GLFW");
-        glfwTerminate();
-        return 1;
+    //caso inicial para crear el cono
+    vertices.push_back(0.0);
+    vertices.push_back(0.5);
+    vertices.push_back(0.0);
+    
+    //ciclo for para crear los vÈrtices de la circunferencia del cono
+    for (n = 0; n <= (res); n++) {
+        x = R * cos((n)*dt);
+        z = R * sin((n)*dt);
+        for (i = 0; i < 3; i++) {
+            switch (i) {
+            case 0:
+                vertices.push_back(x);
+                break;
+            case 1:
+                vertices.push_back(y);
+                break;
+            case 2:
+                vertices.push_back(z);
+                break;
+            }
+        }
     }
-    //Obtener tamaño de Buffer
-    int BufferWidth, BufferHeight;
-    glfwGetFramebufferSize(mainWindow, &BufferWidth, &BufferHeight);
+    vertices.push_back(R * cos(0) * dt);
+    vertices.push_back(-0.5);
+    vertices.push_back(R * sin(0) * dt);
 
-    //asignar el contexto
-    glfwMakeContextCurrent(mainWindow);
 
-    //permitir nuevas extensiones
-    glewExperimental = GL_TRUE;
+    for (i = 0; i < res+2; i++) indices.push_back(i);
 
-    if (glewInit() != GLEW_OK)
-    {
-        printf("Falló inicialización de GLEW");
-        glfwDestroyWindow(mainWindow);
-        glfwTerminate();
-        return 1;
-    }
+    //se genera el mesh del cono
+    Mesh *cono = new Mesh();
+    cono->CreateMeshGeometry(vertices, indices, vertices.size(), res + 2);
+    meshList.push_back(cono);
+}
 
-    // Asignar valores de la ventana y coordenadas
-    //Asignar Viewport
-    glViewport(0, 0, BufferWidth, BufferHeight);
-    printf("Version de Opengl: %s \n",glGetString(GL_VERSION));
-    printf("Marca: %s \n", glGetString(GL_VENDOR));
-    printf("Renderer: %s \n", glGetString(GL_RENDERER));
-    printf("Shaders: %s \n", glGetString(GL_SHADING_LANGUAGE_VERSION));
+//funciÛn para crear pir·mide cuadrangular unitaria
+void CrearPiramideCuadrangular()
+{
+    vector<unsigned int> piramidecuadrangular_indices = {
+        0,3,4,
+        3,2,4,
+        2,1,4,
+        1,0,4,
+        0,1,2,
+        0,2,4
+
+    };
+    vector<GLfloat> piramidecuadrangular_vertices = {
+        0.5f,-0.5f,0.5f,
+        0.5f,-0.5f,-0.5f,
+        -0.5f,-0.5f,-0.5f,
+        -0.5f,-0.5f,0.5f,
+        0.0f,0.5f,0.0f,
+    };
+    Mesh *piramide = new Mesh();
+    piramide->CreateMeshGeometry(piramidecuadrangular_vertices, piramidecuadrangular_indices, 15, 18);
+    meshList.push_back(piramide);
+}
+
+
+
+void CreateShaders()
+{
+    Shader *shader1 = new Shader();
+    shader1->CreateFromFiles(vShader, fShader);
+    shaderList.push_back(*shader1);
+
+    Shader* shader2 = new Shader();
+    shader2->CreateFromFiles(vShaderColor, fShader);
+    shaderList.push_back(*shader2);
+}
+
+
+int main()
+{
+    mainWindow = Window(800, 600);
+    mainWindow.Initialise();
+    //Cilindro y cono reciben resoluciÛn (slices, rebanadas) y Radio de circunferencia de la base y tapa
+
+    CrearCubo();//Ìndice 0 en MeshList
+    CrearPiramideTriangular();//Ìndice 1 en MeshList
+    CrearCilindro(5, 1.0f);//Ìndice 2 en MeshList
+    CrearCono(25, 2.0f);//Ìndice 3 en MeshList
+    CrearPiramideCuadrangular();//Ìndice 4 en MeshList
+    CreateShaders();
+    
+    
+
+    /*C·mara se usa el comando: glm::lookAt(vector de posiciÛn, vector de orientaciÛn, vector up));
+    En la clase Camera se reciben 5 datos:
+    glm::vec3 vector de posiciÛn,
+    glm::vec3 vector up,
+    GlFloat yaw rotaciÛn para girar hacia la derecha e izquierda
+    GlFloat pitch rotaciÛn para inclinar hacia arriba y abajo
+    GlFloat velocidad de desplazamiento,
+    GlFloat velocidad de vuelta o de giro
+    Se usa el Mouse y las teclas WASD y su posiciÛn inicial est· en 0,0,1 y ve hacia 0,0,-1.
+    */
+
+    camera = Camera(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f), -60.0f, 0.0f, 0.3f, 0.3f);
+
+    
+    GLuint uniformProjection = 0;
+    GLuint uniformModel = 0;
+    GLuint uniformView = 0;
+    GLuint uniformColor = 0;
+    glm::mat4 projection = glm::perspective(glm::radians(60.0f)    ,mainWindow.getBufferWidth() / mainWindow.getBufferHeight(), 0.1f, 100.0f);
+    //glm::mat4 projection = glm::ortho(-1, 1, -1, 1, 1, 10);
+    
     //Loop mientras no se cierra la ventana
-    while (!glfwWindowShouldClose(mainWindow))
+    sp.init(); //inicializar esfera
+    sp.load();//enviar la esfera al shader
+
+    glm::mat4 model(1.0);//Inicializar matriz de Modelo 4x4
+
+    glm::vec3 color = glm::vec3(0.0f,0.0f,0.0f); //inicializar Color para enviar a variable Uniform;
+
+    while (!mainWindow.getShouldClose())
     {
+        
+        GLfloat now = glfwGetTime();
+        deltaTime = now - lastTime;
+        deltaTime += (now - lastTime) / limitFPS;
+        lastTime = now;
         //Recibir eventos del usuario
         glfwPollEvents();
+        //C·mara
+        camera.keyControl(mainWindow.getsKeys(), deltaTime);
+        camera.mouseControl(mainWindow.getXChange(), mainWindow.getYChange());
 
         //Limpiar la ventana
-        glClearColor(0.0f,1.0f,0.0f,1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //Se agrega limpiar el buffer de profundidad
+        shaderList[0].useShader();
+        uniformModel = shaderList[0].getModelLocation();
+        uniformProjection = shaderList[0].getProjectLocation();
+        uniformView = shaderList[0].getViewLocation();
+        uniformColor = shaderList[0].getColorLocation();
+        
+        model = glm::mat4(1.0);
+        //TraslaciÛn inicial para posicionar en -Z a los objetos
+        model = glm::translate(model, glm::vec3(0.0f, 0.0f, -4.0f));
+        //otras transformaciones para el objeto
+        //model = glm::scale(model, glm::vec3(0.5f,0.5f,0.5f));
+        model = glm::rotate(model, glm::radians(mainWindow.getrotax()), glm::vec3(1.0f, 0.0f, 0.0f));
+        model = glm::rotate(model, glm::radians(mainWindow.getrotay()), glm::vec3(0.0f, 1.0f, 0.0f));  //al presionar la tecla Y se rota sobre el eje y
+        model = glm::rotate(model, glm::radians(mainWindow.getrotaz()), glm::vec3(0.0f, 0.0f, 1.0f));
+        glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+        //la lÌnea de proyecciÛn solo se manda una vez a menos que en tiempo de ejecuciÛn
+        //se programe cambio entre proyecciÛn ortogonal y perspectiva
+        glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
+        glUniformMatrix4fv(uniformView, 1, GL_FALSE, glm::value_ptr(camera.calculateViewMatrix()));
+        color = glm::vec3(1.0f, 0.0f, 1.0f);
+        glUniform3fv(uniformColor, 1, glm::value_ptr(color)); //para cambiar el color del objetos
+        meshList[0]->RenderMesh(); //dibuja cubo y pir·mide triangular
+        //meshList[3]->RenderMeshGeometry(); //dibuja las figuras geomÈtricas cilindro, cono, pir·mide base cuadrangular
+        //sp.render(); //dibuja esfera
+        
+        
+        /*
+        //ejercicio: Instanciar primitivas geomÈtricas para recrear el dibujo de la pr·ctica pasada en 3D,
+        //se requiere que exista piso y la casa tiene una ventana azul circular justo en medio de la pared trasera y solo 1 puerta frontal.
+        model = glm::mat4(1.0f);
+        color=glm::vec3(0.0f,1.0f,0.0f);
+        //Opcional duplicar esta traslaciÛn inicial para posicionar en -Z a los objetos en el mismo punto
+        //model = glm::translate(model, glm::vec3(0.0f, 0.0f, -3.0f));
+        model = glm::translate(model, glm::vec3(0.0f, 0.75f, -2.5f));
+        model = glm::scale(model, glm::vec3(0.5f, 0.5f, 1.0f));
+        glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));//FALSE ES PARA QUE NO SEA TRANSPUESTA
+        meshList[1]->RenderMeshGeometry();
+        */
 
-        glfwSwapBuffers(mainWindow);
-
+        glUseProgram(0);
+        mainWindow.swapBuffers();
     }
-
-
     return 0;
 }
+
+    
+        
